b2b51d75fd229a3e40fca9ba33fb28887dea0bbf
AndroidDeveloperLB <lblb636@gmail.com>
2019-01-30 15:53:56 +0200

    updated stuff

--- src/com/lb/screenshot_sample/MainActivity.java
+++ src/com/lb/screenshot_sample/MainActivity.java
@@ -1,17 +1,18 @@
 package com.lb.screenshot_sample;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Toast;
 
 import com.mtsahakis.mediaprojectiondemo.ScreenshotManager;
 
-
-public class MainActivity extends Activity {
-
+public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_ID = 1;
 
     @Override
@@ -27,7 +28,7 @@ public class MainActivity extends Activity {
         findViewById(R.id.takeScreenshotButton).setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(final View v) {
-                if(!ScreenshotManager.INSTANCE.takeScreenshot(MainActivity.this))
+                if (!ScreenshotManager.INSTANCE.takeScreenshot(MainActivity.this))
                     Toast.makeText(MainActivity.this, "failed to take screenshot for some reason", Toast.LENGTH_SHORT).show();
             }
         });
@@ -37,5 +38,34 @@ public class MainActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_ID)
             ScreenshotManager.INSTANCE.onActivityResult(resultCode, data);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        String url = null;
+        switch (item.getItemId()) {
+            case R.id.menuItem_all_my_apps:
+                url = "https://play.google.com/store/apps/developer?id=AndroidDeveloperLB";
+                break;
+            case R.id.menuItem_all_my_repositories:
+                url = "https://github.com/AndroidDeveloperLB";
+                break;
+            case R.id.menuItem_current_repository_website:
+                url = "https://github.com/AndroidDeveloperLB/ScreenshotSample";
+                break;
+        }
+        if (url == null)
+            return true;
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        startActivity(intent);
+        return true;
     }
 }
